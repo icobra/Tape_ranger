ecbbdc71e4f8764506e8d99c87ae2f39cdbcc5d7
Sasha Good aka icobra <eiroglif@mail.ru>
2021-07-23 17:03:09 +0300

    Add description to db module

---
+++ modules/db_inspector.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+'''
+Module to controll JSON db 
+
+db scheme
+
+anime = [
+    {"Name":"Title_name/catalog_name", "md5-sum":"hash", "tape1":"mnt_name", "tape2":"mnt_name"},
+]
+
+'''
+
+import json
--- tape_ranger.py
+++ tape_ranger.py
@@ -7,11 +7,13 @@ Tape ranger is a simple
 
 # Utilite copy date to tape 
 
-# Utilite with DB 
+# Utilite with DB -- db_inspector.py
+
+# JSON db store in catalog /date
 
 # Utilite check that user made 2 copy 
 
-# Utilite have a MD5 sum of original files
+# Utilite have a MD5 sum of original files -- md5_inspector.py
 
 
 def main():
