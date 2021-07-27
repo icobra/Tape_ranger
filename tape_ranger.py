0af330e8ed7e036d1077a994d8d167292e874d92
Sasha Good aka icobra <eiroglif@mail.ru>
2021-07-27 16:48:00 +0300

    Feat add Sorted class pseudocode

--- tape_ranger.py
+++ tape_ranger.py
@@ -4,6 +4,22 @@ Tape ranger is a simple
 '''
 
 # Utilite sorted new files and folders
+    # In enter we have folder with other files and folders
+    # class Sorted (? not best name)
+        # def __init__(self):
+            # self.dir_list = dir_list
+
+        #def scan_dir(unsorted_dir) -> 'list':
+            # Take all name and return list of all_name
+
+        #def rename_anime(self.dir_list) -> 'list':
+            # Rename anime if user want and return list
+
+        #def copy anime(self.dir_list, sorted_dir) -> 'list':
+            # Copy anime to sorted dir
+            # Del unsorted files and folders
+            # Clean self.dir_list 
+
 
 # Utilite copy date to tape 
 
